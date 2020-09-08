965abcdabda8fcbbd9c12c6417b2bab3eb75fc56
John-Osiko <johnmaxosiko@gmail.com>
2020-09-08 08:29:43 +0300

    Added a class for test and docstring

--- password_test.py
+++ password_test.py
@@ -3,3 +3,12 @@ from password import Password
 import pyperclip
 import uuid
 
+class TestPassword(unittest.TestCase):
+    """
+    Test class that defines test cases for the password class behaviours
+
+    Args:
+        unittest : TestCase class that helps in creating test cases
+    """
+
+   
