760711ccc65b9ccd8b714d055f293854ed5a29ec
John-Osiko <johnmaxosiko@gmail.com>
2020-09-08 08:32:11 +0300

    Added a test to confirm initialization of the class object

--- password_test.py
+++ password_test.py
@@ -18,4 +18,14 @@ class TestPassword(unittest.TestCase):
         self.new_password = Password("Evernote", "John", "Osiko", "johnmaxosiko@gmail.com", "projecttest1")
 
 
+    def test_init(self):
+        """
+        test__init test case to test if the my_password object is initialized properly
+        """
+        self.assertEqual(self.new_password.web_site, "Evernote")
+        self.assertEqual(self.new_password.first_name, "John")
+        self.assertEqual(self.new_password.last_name, "Osiko")
+        self.assertEqual(self.new_password.user_name, "johnmaxosiko@gmail.com")
+        self.assertEqual(self.new_password.user_password, "projecttest1")
+
     
