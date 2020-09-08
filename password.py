11f418177d95a473cb686b61f92fdacc6c202a5a
John-Osiko <johnmaxosiko@gmail.com>
2020-09-08 08:18:59 +0300

    Added an object to class, initialized and instantiated the object

--- password.py
+++ password.py
@@ -1,4 +1,16 @@
 import pyperclip
 import uuid
 class Password:
+    """
+    Class that generates new instances of passwords
+    """
+    my_password = []
+
+    def __init__(self, web_site, first_name, last_name, user_name, user_password):
+        self.web_site = web_site
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_name = user_name
+        self.user_password = user_password
+
     
